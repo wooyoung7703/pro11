9430af4556159ee88868ba545059d5f504932e95
wooyoung <wooyoung7703@gmail.com>
2025-10-12 19:19:33 +0900

    kkkk

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -106,6 +106,7 @@ const adminNav: SimpleNav[] = [
 ];
 
 function isLinkActive(item: SimpleNav) {
+  alert('aaaaaaaaaaa0000aa');
   if (item.match) return item.match(route);
   return route.path === item.to;
 }
