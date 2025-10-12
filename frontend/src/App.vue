3fbca9ef6df226b009e2ed0dd4829def5ffc4f10
wooyoung <wooyoung7703@gmail.com>
2025-10-12 17:40:50 +0900

    add alert

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -106,6 +106,7 @@ const adminNav: SimpleNav[] = [
 ];
 
 function isLinkActive(item: SimpleNav) {
+  alert('test2')
   if (item.match) return item.match(route);
   return route.path === item.to;
 }
